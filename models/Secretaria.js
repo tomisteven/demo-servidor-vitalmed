448c4cc27da0e5a47bcd304fb93de18021b8dd96
Tomas Steven Desktop <tomssteven22@gmail.com>
2025-12-05 12:51:57 -0300

    first commit

---
+++ models/Secretaria.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SecretariaSchema = new Schema({
+  nombre: { type: String, required: true },
+  usuario: String,
+  password: {
+    type: String,
+    required: true,
+  },
+  telefono: String,
+  email: String,
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  rol: { type: String, default: "Secretaria" },
+});
+
+module.exports = mongoose.model("Secretaria", SecretariaSchema);
